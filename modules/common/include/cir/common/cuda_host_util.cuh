a7a0f1cd37016fb66e49f822ddcefa6793306cf8
f00sek <biale.jerozy@gazeta.pl>
2014-08-16 20:10:56 +0200

    [common] CUDA host utils

---
+++ modules/common/include/cir/common/cuda_host_util.cuh
@@ -0,0 +1,11 @@
+#ifndef CUDA_INIT_CUH_
+#define CUDA_INIT_CUH_
+
+namespace cir { namespace common {
+
+void cuda_init();
+void cuda_shutdown();
+
+}}
+
+#endif
---
+++ modules/common/src/common/cuda_host_util.cu
@@ -0,0 +1,13 @@
+#include "cir/common/cuda_host_util.cuh"
+
+namespace cir { namespace common {
+
+void cuda_init() {
+
+}
+
+void cuda_shutdown() {
+
+}
+
+}}
