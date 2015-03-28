c4c605746a0523d3ea649e18de29dfbe20bddb6b
f00sek <biale.jerozy@gazeta.pl>
2015-03-28 20:09:14 +0100

    [experiment] Metro CPU experiment.

--- metro-recognition.cpp
+++ metro-recognition.cpp
@@ -2,11 +2,14 @@
 #include "cir/gpuprocessing/GpuImageProcessingService.h"
 #include "cir/gpuprocessing/GpuUnionFindSegmentator.h"
 #include "cir/common/logger/ImmediateConsoleLogger.h"
+#include "cir/common/logger/BufferedConfigurableLogger.h"
 #include "cir/common/logger/NullLogger.h"
 #include "cir/common/test_file_loader.h"
 #include "cir/common/recognition/MetroRecognizor.h"
 #include "opencv2/opencv.hpp"
 #include "opencv2/gpu/gpu.hpp"
+#include <string>
+#include <list>
 
 using namespace cir::common;
 using namespace cir::common::logger;
@@ -14,24 +17,50 @@ using namespace cir::common::recognition;
 using namespace cir::cpuprocessing;
 using namespace cir::gpuprocessing;
 
+void showRecognitionResults(MetroRecognizor& recognizor, ImageProcessingService* service);
 void recognize(std::string filePath, MetroRecognizor& recognizor, ImageProcessingService* service);
+void experiment(MetroRecognizor& recognizor, ImageProcessingService* service);
 
 int main() {
-	ImmediateConsoleLogger logger;
-	GpuImageProcessingService service(logger);
-	service.setSegmentator(new GpuUnionFindSegmentator);
+	std::list<std::string> loggerConf;
+	loggerConf.push_back("Count Hu moments");
+	BufferedConfigurableLogger logger(loggerConf);
+	CpuImageProcessingService service(logger);
+//	service.setSegmentator(new GpuUnionFindSegmentator);
+	service.init(0, 0);
+	service.setSegmentatorMinSize(30);
 
 	MetroRecognizor recognizor(service);
 	recognizor.learn(getTestFile("metro", "metro.png").c_str());
 
+//	showRecognitionResults(recognizor, &service);
+	experiment(recognizor, &service);
+
+	logger.flushBuffer();
+}
+
+void showRecognitionResults(MetroRecognizor& recognizor, ImageProcessingService* service) {
 	cv::namedWindow("result");
-	recognize(getTestFile("metro", "metro.png"), recognizor, &service);
-	recognize(getTestFile("metro", "warszawa_metro_swietokrzyska.jpeg"), recognizor, &service);
-	recognize(getTestFile("metro", "metro_warszawa_450.jpeg"), recognizor, &service);
-	recognize(getTestFile("metro", "metro_mlociny.jpeg"), recognizor, &service);
-	recognize(getTestFile("metro", "metro-imielin.jpg"), recognizor, &service);
-	recognize(getTestFile("metro", "metro-otwarte.jpeg"), recognizor, &service);
-	recognize(getTestFile("metro", "Metro_stoklosy.jpg"), recognizor, &service);
+	recognize(getTestFile("metro", "metro.png"), recognizor, service);
+	recognize(getTestFile("metro", "warszawa_metro_swietokrzyska.jpeg"), recognizor, service);
+	recognize(getTestFile("metro", "metro_warszawa_450.jpeg"), recognizor, service);
+	recognize(getTestFile("metro", "metro_mlociny.jpeg"), recognizor, service);
+	recognize(getTestFile("metro", "metro-imielin.jpg"), recognizor, service);
+	recognize(getTestFile("metro", "metro-otwarte.jpeg"), recognizor, service);
+	recognize(getTestFile("metro", "Metro_stoklosy.jpg"), recognizor, service);
+}
+
+void experiment(MetroRecognizor& recognizor, ImageProcessingService* service) {
+	int reps = 100;
+	for(int i = 0; i < reps; i++) {
+		recognize(getTestFile("metro", "metro.png"), recognizor, service);
+	}
+	for(int i = 0; i < reps; i++) {
+		recognize(getTestFile("metro", "metro640x480.png"), recognizor, service);
+	}
+	for(int i = 0; i < reps; i++) {
+		recognize(getTestFile("metro", "metro1024x768.png"), recognizor, service);
+	}
 }
 
 void recognize(std::string filePath, MetroRecognizor& recognizor, ImageProcessingService* service) {
@@ -43,5 +72,5 @@ void recognize(std::string filePath, MetroRecognizor& recognizor, ImageProcessin
 	}
 	mat = service->getMat(mw);
 	cv::imshow("result", mat);
-	cv::waitKey(0);
+//	cv::waitKey(0);
 }
